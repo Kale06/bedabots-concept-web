c62a3b40fef06164ad516fd14daec0dfa5d58d72
Muhammad Haikal <haikaluwu21@gmail.com>
2024-03-31 05:02:18 +0800

    Finalized

--- main.js
+++ main.js
@@ -1,10 +1,30 @@
-import { setupCounter } from './counter.js'
-import navbar from './src/navbar.js'
+import $ from 'jquery';
+import gsap from 'gsap';
 
+/**
+ * @author kale
+ * @description Upon page start function.
+ */
 function init() {
-  document.querySelector('#app').innerHTML = `
-      ${navbar}
-  `
+  $(document).ready(function () {
+    animateElements();
+  });
+}
+
+/**
+ * @author kale
+ * @description Animate elements on page load.
+ */
+function animateElements() {
+  // gsap.from($('.char') , { opacity: 0, duration: 1, y: 100, stagger: 0.3})
+  gsap.from($('#freezetess'), { opacity: 0, duration: 1, y: 100 });
+  gsap.from($('#shockitty'), { opacity: 0, duration: 1, x: -100 });
+  gsap.from($('#smirkitsune'), { opacity: 0, duration: 1, x: 100 });
+  gsap.from($('#logo'), { opacity: 0, duration: 1, y: 250 });
+  gsap.from($('#desktop'), { opacity: 0, duration: 2.5 });
+  gsap.from($('#portable'), { opacity: 0, duration: 2.5 });
 }
 
 init();
+
+export default init;
--- src/tophero.js
+++
@@ -1,8 +0,0 @@
-let topHero = `
-<div class="top-hero">
-    <img src="" alt="bannerImage" class="hero">
-    img src="" alt="logoImage" class="hero">
-</div>
-`
-
-export default topHero;
